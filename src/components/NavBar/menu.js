dda178dc481bd7da95f160a0131f42007f5eaa87
alexnberlin <93226100+alexsaav@users.noreply.github.com>
2023-07-17 18:34:53 +0200

    refactor menu.js

--- src/components/NavBar/menu.js
+++ src/components/NavBar/menu.js
@@ -19,50 +19,7 @@ const menuList = [
                 url: "/movies/top-rated"
             }
         ]
-    },
-    /* {
-        title: "Popular People",
-        url: "/popular-people"
-    }, */
+    }
 ];
 export default menuList
 
-
-/* {
-    title: "TV Series",
-    items: [
-        {
-            name: "Popular",
-            url: "",
-        },
-        {
-            name: "Airing Today",
-            url: ""
-        },
-        {
-            name: "On TV",
-            url: ""
-        },
-        {
-            name: "Top Rated",
-            url: ""
-        }
-    ]
-},
-{   
-    title: "People",
-    items: [
-        {
-            name: "Popular People",
-            url: ""
-        }
-    ]
-},
-{
-    title: "Login",
-    url: "/user/login"
-},
-{
-    title: "Register",
-    url: "/user/register"
-} */
