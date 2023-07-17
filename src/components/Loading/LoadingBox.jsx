32bc0b6f8d662741386dec34c8e6abffcd066880
alexnberlin <93226100+alexsaav@users.noreply.github.com>
2023-07-17 16:42:55 +0200

    refactor LoadingBox component

--- src/components/Loading/LoadingBox.jsx
+++ src/components/Loading/LoadingBox.jsx
@@ -1,49 +1,12 @@
 import React from 'react'
 import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
+import { loadingBoxStyles } from './loadingItems';
 import { v4 as uuidv4 } from 'uuid';
-
-const loadingBoxStyles = {
-    outerContainer: {
-        display: "flex",
-        justifyContent: "space-around", 
-        alignItems: "center",
-        p: "40px 30px",
-        background: "#36393A"
-    },
-    innerContainer: {
-        display: "flex", 
-        flexDirection: "column", 
-        p: "0px 10px", 
-        width: "600px"
-    },
-    textContainer: {
-        display: "flex", 
-        flexDirection: "column", 
-        justifyContent: "center", 
-        ml: 1, 
-        mt: "7px"
-    },
-    pictureSkeletonContainer: {
-        display: "flex",
-        width: "35%"
-    },
-    skeletonSubtitle: {
-        width: "100%",
-        height: 20,
-        mb: "10px"
-    },
-    skeletonText: {
-        width: "100%",
-        height: 70,
-        mb: "10px"
-    },
-
-}
 
 const LoadingBox = (items) => {
     const loadingItem = Array(items).fill().map(() =>
-        <Box sx={loadingBoxStyles.textContainer}>
+        <Box sx={loadingBoxStyles.textContainer} key={uuidv4()}>
             <Skeleton animation="wave" variant="h2" sx={loadingBoxStyles.skeletonSubtitle}/>
             <Skeleton animation="wave" variant="rectangle" sx={loadingBoxStyles.skeletonText}/>
         </Box>
