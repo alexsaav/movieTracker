1b74b1f602956a52c9e445df3ea570d85e6007fd
alexnberlin <93226100+alexsaav@users.noreply.github.com>
2023-07-17 16:42:26 +0200

    add loadingBox styles

---
+++ src/components/Loading/loadingItems.js
@@ -0,0 +1,36 @@
+export const loadingBoxStyles = {
+    outerContainer: {
+        display: "flex",
+        justifyContent: "space-around", 
+        alignItems: "center",
+        p: "40px 30px",
+        background: "#36393A"
+    },
+    innerContainer: {
+        display: "flex", 
+        flexDirection: "column", 
+        p: "0px 10px", 
+        width: "600px"
+    },
+    textContainer: {
+        display: "flex", 
+        flexDirection: "column", 
+        justifyContent: "center", 
+        ml: 1, 
+        mt: "7px"
+    },
+    pictureSkeletonContainer: {
+        display: "flex",
+        width: "35%"
+    },
+    skeletonSubtitle: {
+        width: "100%",
+        height: 20,
+        mb: "10px"
+    },
+    skeletonText: {
+        width: "100%",
+        height: 70,
+        mb: "10px"
+    }
+};
