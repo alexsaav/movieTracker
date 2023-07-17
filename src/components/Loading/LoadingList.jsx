9f4236e642e57f4ee84b03c2ae1371887e750608
alexnberlin <93226100+alexsaav@users.noreply.github.com>
2023-07-17 17:00:57 +0200

    refactor LoadingList component

--- src/components/Loading/LoadingList.jsx
+++ src/components/Loading/LoadingList.jsx
@@ -1,17 +1,17 @@
 import Skeleton from "@mui/material/Skeleton";
 import Box from "@mui/material/Box";
 import { v4 as uuidv4 } from 'uuid';
-import { loadingList } from "./loadingItems";
+import { loadingListStyles } from "./loadingItems";
 
 const LoadingList = ({items}) => {
 
     return (
         <>
             {Array(items).fill().map(() =>
-                (<Box sx={loadingList.container} key={uuidv4()}>
+                (<Box sx={loadingListStyles.container} key={uuidv4()}>
                     <Skeleton animation="wave" variant="circular" width={56} height={56} />
-                    <Box sx={loadingList.innerBox}>
-                        <Skeleton animation="wave" variant="h1" height={15} width={200} sx={loadingList.innerBoxItem} />
+                    <Box sx={loadingListStyles.innerBox}>
+                        <Skeleton animation="wave" variant="h1" height={15} width={200} sx={loadingListStyles.innerBoxItem} />
                         <Skeleton animation="wave" variant="h2" height={15} width={200} /> 
                     </Box>
                 </Box>
