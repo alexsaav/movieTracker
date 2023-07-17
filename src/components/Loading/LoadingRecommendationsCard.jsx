8f642e0cae6077bcbc0fd926dec7d4318647c9f5
alexnberlin <93226100+alexsaav@users.noreply.github.com>
2023-07-17 17:04:23 +0200

    refactor LoadingRecommendationsCard component

--- src/components/Loading/LoadingRecommendationsCard.jsx
+++ src/components/Loading/LoadingRecommendationsCard.jsx
@@ -1,22 +1,28 @@
 import { Box } from '@mui/system';
 import Skeleton from '@mui/material/Skeleton';
 import { v4 as uuidv4 } from 'uuid';
+import { loadingRecommendationCardStyles } from './loadingItems';
 
 export const LoadingRecommendationCard = () => {
     return (
         <>
             {
                 Array(6).fill().map(() =>
-                    <Box>
+                    <Box key={uuidv4()}>
                         <Skeleton 
                             animation="wave" 
                             variant="rectangular" 
                             width={260} 
                             height={146} 
-                            sx={{mr: 1, borderRadius: 2}} 
+                            sx={loadingRecommendationCardStyles.firstItem} 
                         />
                         <Box>
-                            <Skeleton animation="wave" height={10} width="90%" sx={{borderRadius: 1}}/>
+                            <Skeleton 
+                                animation="wave" 
+                                height={10} 
+                                width="90%" 
+                                sx={loadingRecommendationCardStyles.secondItem}
+                            />
                         </Box>
                     </Box>
                 )
