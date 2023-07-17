622866b68a22091df93b221c9474eaf24f647243
alexnberlin <93226100+alexsaav@users.noreply.github.com>
2023-07-17 16:52:34 +0200

    refactor LoadingCardItem component

--- src/components/Loading/LoadingCardItem.jsx
+++ src/components/Loading/LoadingCardItem.jsx
@@ -1,17 +1,18 @@
 import Skeleton from "@mui/material/Skeleton";
 import Box from "@mui/material/Box";
 import { v4 as uuidv4 } from 'uuid';
+import { loadingCardItem } from "./loadingItems";
 
 const LoadingCardItem = ({items}) => {
 
     return (
         <>
             {Array(items).fill().map(() =>
-                <Box sx={{width: 150, position: "relative"}} key={uuidv4()}>
-                    <Skeleton animation="wave" variant="rectangular" height={225} sx={{mr: 1, borderRadius: 2}} />
+                <Box sx={loadingCardItem.container} key={uuidv4()}>
+                    <Skeleton animation="wave" variant="rectangular" height={225} sx={loadingCardItem.firstItem} />
                     <Box>
-                        <Skeleton animation="wave" height={20} width="70%" sx={{borderRadius: 2}}/>
-                        <Skeleton animation="wave" height={20} width="70%" sx={{borderRadius: 2}}/>
+                        <Skeleton animation="wave" height={20} width="70%" sx={loadingCardItem.secondItemInner}/>
+                        <Skeleton animation="wave" height={20} width="70%" sx={loadingCardItem.secondItemInner}/>
                     </Box>
                 </Box>
             )}
